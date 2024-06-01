df5e4a585203327fc1034f60c038397101223926
denux209 <147421147+denux209@users.noreply.github.com>
2024-06-01 13:19:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_discord= request.form.get('button_discord')
+    button_html= request.form.get('button_html')
+    button_db= request.form.get('button_db')
+    button_python = request.form.get('button_python')
+
+    return render_template('index.html',
+                           button_python=button_python,
+                           button_discord=button_discord,
+                           button_html=button_html,
+                           button_db=button_db)
+@app.route("/feedback",methods=["POST"])
+def feedback():
+    email = request.form.get("email")
+    text=request.form.get("text")
+    print(email,text)
+    return render_template("feedback.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
